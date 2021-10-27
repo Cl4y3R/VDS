c01a2c2b2836aaa7c88de00a53df5050e4d57646
Cl4y3r <unterwegs_x@outlook.com>
2021-10-27 14:53:46 +0800

    normal update

--- autodriving/src/msg_subpub.cpp
+++ autodriving/src/msg_subpub.cpp
@@ -8,7 +8,7 @@
 
 
 // 自定义类继承该基类
-#include "rclcpp/rclcpp.hpp"
+#include "rclcpp/rclcpp.hpp" //rclcpp是ROS2的C++API
 #include "std_msgs/msg/string.hpp"
 
 // 消息类型-订阅
@@ -99,13 +99,61 @@ void msgSubPub::subscriber_callback(const sensor_msgs::msg::CompressedImage::Con
 // 发布者的回调函数
 void msgSubPub::publisher_callback()
 {
-    // 车辆控制
+    /*车辆控制
+      float32 acceleration_pct  # 0 to 1
+      float32 braking_pct  # 0 to 1
+      float32 target_wheel_angle  # radians
+      float32 target_wheel_angular_rate  # radians / second
+      uint8 target_gear
+
+      uint8 GEAR_NEUTRAL = 0
+      uint8 GEAR_DRIVE = 1
+      uint8 GEAR_REVERSE = 2
+      uint8 GEAR_PARKING = 3
+      uint8 GEAR_LOW = 4*/
     auto control = lgsvl_msgs::msg::VehicleControlData();
     control.target_gear = lgsvl_msgs::msg::VehicleControlData::GEAR_DRIVE; //前进档位
-    control.acceleration_pct = 5;  //加速度
-    // 车辆状态
+    control.acceleration_pct = 0.5;  //加速踏板
+    control.braking_pct = 0; //制动踏板
+    control.target_wheel_angle = 0.26; //车轮转角
+
+    /*车辆状态
+      uint8 blinker_state
+      uint8 headlight_state
+      uint8 wiper_state
+      uint8 current_gear
+      uint8 vehicle_mode
+      bool hand_brake_active
+      bool horn_active
+      bool autonomous_mode_active
+
+      uint8 BLINKERS_OFF = 0
+      uint8 BLINKERS_LEFT = 1
+      uint8 BLINKERS_RIGHT = 2
+      uint8 BLINKERS_HAZARD = 3
+
+      uint8 HEADLIGHTS_OFF = 0
+      uint8 HEADLIGHTS_LOW = 1
+      uint8 HEADLIGHTS_HIGH = 2
+
+      uint8 WIPERS_OFF = 0
+      uint8 WIPERS_LOW = 1
+      uint8 WIPERS_MED = 2
+      uint8 WIPERS_HIGH = 3
+
+      uint8 GEAR_NEUTRAL = 0
+      uint8 GEAR_DRIVE = 1
+      uint8 GEAR_REVERSE = 2
+      uint8 GEAR_PARKING = 3
+      uint8 GEAR_LOW = 4
+
+      uint8 VEHICLE_MODE_COMPLETE_MANUAL = 0
+      uint8 VEHICLE_MODE_COMPLETE_AUTO_DRIVE = 1
+      uint8 VEHICLE_MODE_AUTO_STEER_ONLY = 2
+      uint8 VEHICLE_MODE_AUTO_SPEED_ONLY = 3
+      uint8 VEHICLE_MODE_EMERGENCY_MODE = 4*/
     auto state = lgsvl_msgs::msg::VehicleStateData();
-    state.autonomous_mode_active = true;
+    state.autonomous_mode_active = true; //自动驾驶模式激活
     state.vehicle_mode= lgsvl_msgs::msg::VehicleStateData::VEHICLE_MODE_COMPLETE_AUTO_DRIVE;   //驾驶模式
 
     state_pub->publish(state);
